9a0e70ee760e5abc01b59b606e15ad9b0d013f4f
Reham244 <84594269+Reham244@users.noreply.github.com>
2023-05-22 01:09:25 +0300

    Update Service.java

--- src/main/java/Service/Service.java
+++ src/main/java/Service/Service.java
@@ -1,5 +1,8 @@
 package Service;
-
-public class Service {
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+@ApplicationPath("/api")
+public class Service extends Application {
+	//Service empty class 
 
 }
