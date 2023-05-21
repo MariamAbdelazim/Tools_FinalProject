f9a305a960d9ef383f8483e66120f6d2a062e42f
Maryam Elkadi <119524599+20206160@users.noreply.github.com>
2023-05-22 02:19:28 +0300

    Update RunnerService.java

--- src/main/java/Service/RunnerService.java
+++ src/main/java/Service/RunnerService.java
@@ -1,14 +1,18 @@
-@Stateless
-package Service;
+//package Service;
+
+import Service.EntityManager;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
-//import EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Column;
 import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+import javax.validation.constraints.NotNull;
+ 
 
+@Stateless
 public class runnerService {
     @PersistenceContext
     private EntityManager em;
