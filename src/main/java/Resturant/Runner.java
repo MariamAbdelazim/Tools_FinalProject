7c649309383d48a1a672fc9e281b2de4ac05d748
Maryam Elkadi <119524599+20206160@users.noreply.github.com>
2023-05-22 12:54:58 +0300

    Update Runner.java

--- src/main/java/Resturant/Runner.java
+++ src/main/java/Resturant/Runner.java
@@ -1,29 +1,43 @@
 
-@Stateless
-public class RunnerController {
+package Resturant;
+import Entities.OrderEntity;
+import Entities.RunnerEntity;
+import javax.annotation.Untainted;
+import javax.management.Query;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Order;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+
+import com.google.inject.Inject;
+
+public class Runner {
+    
     @PersistenceContext
     private EntityManager em;
-    
-    @put
+
+    @PUT
     public void updateRunnerStatus(Long runnerId, String status) {
         Runner runner = em.find(Runner.class, runnerId);
-        runner.setStatus(status);
+        RunnerEntity.setRunner_status(status);
         em.merge(runner);
     }
-    @Get
-    public void delivered { 
-        @Inject private EntityManager em;
+
+    
+        @Inject 
+        @PUT
         public void markOrderAsDelivered(Long orderId, Long runnerId) {
             Order order = em.find(Order.class, orderId);
             Runner runner = em.find(Runner.class, runnerId);
-            order.setOrder_status("delivered");
-            runner.setStatus("available");
+            OrderEntity.setOrder_status("delivered");
+            RunnerEntity.setRunner_status("available");
         }
-        @Get
+      
+        @GET
         public int getNumberOfTripsCompletedByRunner(Long runnerId) {
-            Query query = em.createQuery("SELECT COUNT(o) FROM Order o WHERE o.runner.id = :runnerId AND o.order_status = 'delivered'");
+            final javax.persistence.Query query = em.createQuery("SELECT COUNT(o) FROM Order o WHERE o.runner.id = :runnerId AND o.order_status = 'delivered'");
             query.setParameter("runnerId", runnerId);
             return ((Long) query.getSingleResult()).intValue();
         }
-    }
 }
