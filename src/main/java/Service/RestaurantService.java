369e5d8829df588f4fba3e058880411665953c65
MariamAbdelazim <119529563+MariamAbdelazim@users.noreply.github.com>
2023-05-22 20:45:01 +0300

    Update RestaurantService.java

--- src/main/java/Service/RestaurantService.java
+++ src/main/java/Service/RestaurantService.java
@@ -9,22 +9,30 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
+import org.dom4j.io.DOMWriter;
+
 import Entities.MealEntity;
+import Entities.ResturantEntity;
+import Entities.UserEntity;
 import Resturant.Restaurant;
 
 @Stateless
+//@RolesAllowed({"admin, qa"})
 public class resturantService {
     @PersistenceContext
+    //@Resource
     private EntityManager em;
+    
     @Path("signUp")
     @POST
-    public void signUp_resturant(String name, int id , String role) {
+    public resturantService signUp_resturant(int resturant_id,String resturan_name, String resturant_password) {
         // validate restaurant input
+        return Resturant_signUp(resturant_id,resturan_name,resturant_password);
     }
     @Path("login")
     @POST
-    public Restaurant login_resturant(String name, String password) {
-        return login(name, password);
+    public Restaurant login_resturant(String resturan_name, String resturant_password) {
+        return Resturant_login(resturan_name,resturant_password);
     }
     @GET
     public List<MealEntity> getMeals(int restaurantId) {
