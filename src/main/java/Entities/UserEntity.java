b76551707b647279384dcda737afd872c0febd34
Maryam Elkadi <119524599+20206160@users.noreply.github.com>
2023-05-22 20:30:05 +0300

    Update UserEntity.java

--- src/main/java/Entities/UserEntity.java
+++ src/main/java/Entities/UserEntity.java
@@ -6,14 +6,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@PermitAll
+@RolesAllowed({"user"})
 public class UserEntity {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int  User_id;
+	private static int  User_id;
 	private String User_name;
 	private String User_role;
 	
-	public int getUser_id() {
+	public static int getUser_id() {
 		return User_id;
 	}
 	public void setUser_id(int user_id) {
