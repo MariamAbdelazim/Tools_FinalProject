6b8be91377896483b7bfa530f09d323e0f4a5d91
Salma Hossam elden Hassan Mohamed <88389052+SalmHossam@users.noreply.github.com>
2023-05-22 16:03:04 +0300

    Update MealEntity.java

--- src/main/java/Entities/MealEntity.java
+++ src/main/java/Entities/MealEntity.java
@@ -4,32 +4,36 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import Resturant.Restaurant;
 
 @Entity
 public class MealEntity {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int Meal_id;
-	private String Meal_name;
-	private int Price;
+	private static String Meal_name;
+	private static Double Price;
 	private int fk_restaurantId;
-
+    @ManyToOne
+    private Restaurant restaurant;
 	public int getMeal_id() {
 		return Meal_id;
 	}
 	public void setMeal_id(int meal_id) {
 		Meal_id = meal_id;
 	}
-	public String getMeal_name() {
+	public static String getMeal_name() {
 		return Meal_name;
 	}
 	public void setMeal_name(String meal_name) {
 		Meal_name = meal_name;
 	}
-	public int getPrice() {
+	public static Double getPrice() {
 		return Price;
 	}
-	public void setPrice(int price) {
+	public void setPrice(Double price) {
 		Price = price;
 	}
 	public int getFk_restaurantId() {
